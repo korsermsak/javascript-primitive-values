ca218b7dbde3d9f5f00a0c6c80f3b6eb15583832
sermsak <tu.sermsak@gmail.com>
2024-10-22 01:37:05 +0700

    feat : answer

--- ex-6.js
+++ ex-6.js
@@ -4,3 +4,5 @@ let totalParts = 13000;
 
 let concatSentence;
 let literalSentence;
+concatSentence = "Total:" + " " + totalParts + " " + "Mechine parts";
+literalSentence = `Total: ${totalParts} Mechine parts`
